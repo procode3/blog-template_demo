3e8312823e222fdfc8ab5aeab412a52d8ad4d05f
procode3 <51229609+procode3@users.noreply.github.com>
2022-08-30 14:31:58 +0300

    Initial commit

---
+++ pages/blog/index.tsx
@@ -0,0 +1,36 @@
+import ArticleCard from "../../src/components/ArticleCards/ArticleCard";
+import { SORTED_ARTICLES_BY_DATE } from '../../BLOG_CONSTANTS/_ARTICLES_LIST';
+import { useRouter } from "next/router";
+import { PageLayout } from "../../src/components";
+import { combineClasses } from "../../src/utils/utils";
+
+const Categories = () => {
+    const router = useRouter()
+    const { category } = router.query;
+    const categoryArticles = SORTED_ARTICLES_BY_DATE.filter((each) => each.preview.category === category);
+    const articles = category ? categoryArticles : SORTED_ARTICLES_BY_DATE;    
+
+    return (
+        <PageLayout home>
+            <div className={combineClasses("container mt-10 md:pt-0 px-0 md:px-3", category ? 'pt-10' : 'pt-14')}>
+                {
+                    category ?
+                        <h1 className='px-2 mb-[30px] text-[45px] font-bold' style={{ textTransform: 'capitalize' }}>
+                            {category}
+                            <hr className='mt-[10px]' />
+                        </h1> : null
+                }
+
+                <div className='flex flex-wrap'>
+                    {
+                        articles.map((each, i) => (
+                            <ArticleCard article={each.preview} path={each.path} key={i} />
+                        ))
+                    }
+                </div>
+            </div>
+        </PageLayout>
+    )
+}
+
+export default Categories
